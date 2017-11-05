33e8c2e9580c6dad5db4fe756dc5959ad46c9fb4
WPoole <wpoole94@gmail.com>
2017-11-05 15:22:30 -0500

    Modified mainframe and have match scores working.

---
+++ player match scores/calculate_match_scores.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Calculate the match scores"""
+# -*- encoding: utf-8 -*-
+import os,sys,inspect
+import MySQLdb
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from load_config import read_config, set_env_vars
+
+
+# TODO: if it already exists, update the score instead of throwing dup key error
+FG_3_SCORE = 3
+FG_SCORE = 2
+FT_SCORE = 1
+RB_SCORE = 1.2
+AST_SCORE = 1.5
+BLK_SCORE = 3
+STEAL_SCORE = 3
+TURNOVER_SCORE = -1
+SEASON = 2017
+
+def calculate():
+    """Calculate the scores"""
+    connection = MySQLdb.connect(host = os.environ["host"],    # your host, usually localhost
+                                 user = os.environ["user"],         # your username
+                                 passwd = os.environ["pwd"],  # your password
+                                 db = os.environ["db"])        # name of the data base
+
+    # you must create a Cursor object. It will let
+    # you execute all the queries you need
+    cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * FROM player_matches")
+    result_set = cursor.fetchall()
+
+    for row in result_set:
+        score = (row["FGM"] * FG_SCORE + row["3PM"] * FG_3_SCORE + row["FTM"] * FT_SCORE +\
+                 row["REB"] * RB_SCORE + row["AST"] * AST_SCORE + row["BLK"] * BLK_SCORE +\
+                 row["STL"] * STEAL_SCORE + row["TOV"] * TURNOVER_SCORE)
+        player_id = row["player_match_id"]
+
+        cursor.execute("UPDATE `d2matchb_bball`.`player_matches`" +\
+                       " SET score = " + str(score) + "WHERE player_match_id = " + str(player_id) + ";")
+
+    connection.commit()
+    # print all the first cell of all the rows
+    connection.close()
+
+if __name__ == '__main__':
+    # Db config initialization
+    conf = read_config()
+    set_env_vars(conf)
+
+    calculate()
