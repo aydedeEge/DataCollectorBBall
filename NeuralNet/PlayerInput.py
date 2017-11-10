f3bc96a7078b10546e7cbb15b11609700541cd08
AsherWright <sastreen@hotmail.com>
2017-11-10 03:46:14 -0500

    getting there

---
+++ NeuralNet/PlayerInput.py
@@ -0,0 +1,17 @@
+class PlayerInput:
+    
+    def PlayerInput(self, cScore, gScore, pID, tID):
+        self.careerScore = cScore
+        self.gameScore = gScore
+        self.playerID = pID
+        self.teamID = tID
+
+    def toString(self):
+        return "Player " + str(self.playerID) + ", on team " + str(self.teamID) + "with career score " +\
+        str(self.careerScore) + "had game score " + str(self.gameScore)
+    
+    def __init__(self):
+        self.careerScore = 0
+        self.gameScore = 0
+        self.playerID = 0
+        self.teamID = 0
